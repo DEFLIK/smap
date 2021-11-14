685ea33938b1c503c8ce58f8938a9b87c8d433b5
DEFLIK <52451655+DEFLIK@users.noreply.github.com>
2021-11-15 01:25:48 +0500

    DB
    +added db model
    +primite csv db realisation

--- app.py
+++ app.py
@@ -1,108 +1,83 @@
 from flask import Flask, jsonify, request, render_template, redirect, url_for
+from database.models import *
+import csv
 from flask_sqlalchemy import SQLAlchemy
 from markupsafe import escape
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/klimm/Desktop/smap/test.db'
-# db = SQLAlchemy(app)
-#
-#
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(80), unique=True, nullable=False)
-#     email = db.Column(db.String(120), unique=True, nullable=False)
-#
-#     def __repr__(self):
-#         return '<User %r>' % self.username
-#
-#
-# db.create_all()
-
-features = [
-    {
-        'type': 'Feature',
-        'geometry': {
-            'type': 'Point',
-            'coordinates': [55.75244503863624, 37.62483958203125]
-        },
-        'options': {
-            'preset': 'islands#redIcon'
-        },
-        'properties': {
-            'iconCaption': 1980
-        },
-        'id': 'flex'
-    },
-    {
-        'type': 'Feature',
-        'geometry': {
-            'type': 'Point',
-            'coordinates': [56.11645027501593, 47.451569300863646]
-        },
-        'options': {
-            'preset': 'islands#redIcon'
-        },
-        'properties': {
-            'iconCaption': 222
-        },
-        'id': 'kek'
-    },
-    {
-        'type': 'Feature',
-        'geometry': {
-            'type': 'Point',
-            'coordinates': [56.13971149429552, 47.260143579860504]
-        },
-        'options': {
-            'preset': 'islands#redIcon'
-        },
-        'properties': {
-            'iconCaption': 1940
-        },
-        'id': 'bruh'
-    }
-]
-
-data = {
-    'kek': 'Очень умный и крутой чел',
-    'bruh': 'Немного брух чел',
-    'flex': 'Чрезмерно крутой чел'
-}
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/laureates.db'
+db.init_app(app)
 
 
 @app.route('/', methods=['GET'])
 def get_template():
     if request.content_type == 'application/json':
-        return jsonify(features)
-    return render_template('index.html', cords=features)
+        res = []
+        with open('db.csv', newline='') as csvfile:
+            reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+            first = True
+            for row in reader:
+                if first:
+                    first = False
+                    continue
+                res.append({
+                    'type': 'Feature',
+                    'geometry': {
+                        'type': 'Point',
+                        'coordinates': [row[1], row[2]]
+                    },
+                    'options': {
+                        'preset': 'islands#redIcon'
+                    },
+                    'properties': {
+                        'iconCaption': row[3]
+                    },
+                    'id': row[0]})
+
+        return jsonify(res)
+    return render_template('index.html')
 
 
 @app.route('/', methods=['POST'])
 def add_cords():
     js = request.json
-    new_feature = {
-        'type': 'Feature',
-        'geometry': {
-            'id': f'{len(features)}',
-            'type': 'Point',
-            'coordinates': [int(js['cordX']), int(js['cordY'])]
-        },
-        'options': {
-            'preset': 'islands#redIcon'
-        },
-        'properties': {
-            'iconCaption': int(js['cordYear'])
-        },
-        'id': js['cordId']
-    }
-    features.append(new_feature)
-    data[js['cordId']] = js['dataInfo']
-    return jsonify(features)
+    res = []
+    with open('db.csv', 'a', newline='') as csvfile:
+        file = csv.writer(csvfile, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        file.writerow([js['cordId'], js['cordX'], js['cordY'], js['cordYear'], js['dataInfo']])
+    with open('db.csv', 'r', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        first = True
+        for row in reader:
+            if first:
+                first = False
+                continue
+            res.append({
+                'type': 'Feature',
+                'geometry': {
+                    'type': 'Point',
+                    'coordinates': [row[1], row[2]]
+                },
+                'options': {
+                    'preset': 'islands#redIcon'
+                },
+                'properties': {
+                    'iconCaption': row[3]
+                },
+                'id': row[0]})
+    return jsonify(res)
 
 
 @app.route('/<id>', methods=['GET'])
 def get_info(id: str):
-    return jsonify({'info': data[id]})
+    res = ''
+    with open('db.csv', newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in reader:
+            if row[0] == id:
+                res = row[4]
+
+    return jsonify({'info': res})
 
 
 if __name__ == '__main__':
---
+++ database/models.py
@@ -0,0 +1,92 @@
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+
+class Person(db.Model):
+    id_person = db.Column(db.Integer, primary_key=True)
+    person_full_name = db.Column(db.String(128), nullable=False)
+    award_receiving = db.relationship("AwardReceiving", back_populates="person")
+
+    def __repr__(self):
+        return '<%r %r>' % (self.__class__.__name__, self.id_person)
+
+
+class AwardReceiving(db.Model):
+    id_award_receiving = db.Column(db.Integer, primary_key=True)
+    id_person = db.Column(db.Integer, db.ForeignKey('person.id_person'), nullable=False)
+    id_award = db.Column(db.Integer, db.ForeignKey('award.id_award'), nullable=False)
+    id_organization = db.Column(db.Integer, db.ForeignKey('organization.id_organization'), nullable=False)
+    person = db.relationship("Person", back_populates="award_receiving")
+    award = db.relationship("Award", back_populates="award_receiving")
+    organization = db.relationship("Organization", back_populates="award_receiving")
+
+    def __repr__(self):
+        return '<%r %r>' % (self.__class__.__name__, self.id_award_receiving)
+
+
+class Award(db.Model):
+    id_award = db.Column(db.Integer, primary_key=True)
+    id_award_name = db.Column(db.Integer, db.ForeignKey('award_name.id_award_name'), nullable=False)
+    award_rank = db.Column(db.Integer, nullable=True)
+    id_award_year = db.Column(db.Integer, nullable=False)
+    award_achievement = db.Column(db.String(512), nullable=True)
+    id_knowledge_field = db.Column(db.Integer, db.ForeignKey('knowledge_field.id_knowledge_field'), nullable=False)
+    award_receiving = db.relationship("AwardReceiving", back_populates="award")
+    award_name = db.relationship("AwardName", back_populates="award")
+    award_year = db.relationship("AwardYear", back_populates="award")
+    knowledge_field = db.relationship("KnowledgeField", back_populates="award")
+
+    def __repr__(self):
+        return '<%r %r>' % (self.__class__.__name__, self.id_award)
+
+
+class AwardName(db.Model):
+    id_award_name = db.Column(db.Integer, primary_key=True)
+    award_name = db.Column(db.String(64), nullable=False)
+    award = db.relationship("Award", back_populates="award_name")
+
+    def __repr__(self):
+        return '<%r %r>' % (self.__class__.__name__, self.id_award_name)
+
+
+class AwardYear(db.Model):
+    id_award_year = db.Column(db.Integer, primary_key=True)
+    award_year = db.Column(db.Integer, nullable=False)
+    award = db.relationship("Award", back_populates="award_year")
+
+    def __repr__(self):
+        return '<%r %r>' % (self.__class__.__name__, self.id_award_year)
+
+
+class KnowledgeField(db.Model):
+    id_knowledge_field = db.Column(db.Integer, primary_key=True)
+    knowledge_field_name = db.Column(db.String(64), nullable=False)
+    award = db.relationship("Award", back_populates="knowledge_field")
+
+    def __repr__(self):
+        return '<%r %r>' % (self.__class__.__name__, self.id_knowledge_field)
+
+
+class Organization(db.Model):
+    id_organization = db.Column(db.Integer, primary_key=True)
+    organization_name = db.Column(db.String(128), nullable=False)
+    id_address = db.Column(db.Integer, db.ForeignKey('address.id_address'), nullable=False)
+    award_receiving = db.relationship("AwardReceiving", back_populates="organization")
+    address = db.relationship("Address", back_populates="organization")
+
+    def __repr__(self):
+        return '<%r %r>' % (self.__class__.__name__, self.id_organization)
+
+
+class Address(db.Model):
+    id_address = db.Column(db.Integer, primary_key=True)
+    latitude = db.Column(db.DECIMAL(8, 6), nullable=False)
+    longitude = db.Column(db.DECIMAL(8, 6), nullable=False)
+    city_name = db.Column(db.String(64), nullable=False)
+    region_name = db.Column(db.String(128), nullable=True)
+    organization = db.relationship("Organization", back_populates="address")
+
+    def __repr__(self):
+        return '<%r %r>' % (self.__class__.__name__, self.id_address)
